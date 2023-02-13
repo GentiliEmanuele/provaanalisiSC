a85a5ebeb9261ef48d2888daa5c5f1c72d6e2a48
Emanuele <egentili2000@gmail.com>
2023-02-13 21:37:31 +0100

    Modified main

--- src/main/java/prova/MainClass.java
+++ src/main/java/prova/MainClass.java
@@ -3,5 +3,6 @@ package prova;
 public class MainClass {
     public static void main(String [] args) {
         System.out.println("Hello world");
+        System.out.println("Ciao mondo");
     }
 }
